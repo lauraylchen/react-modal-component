8f43d01d69e5b4c71e4efe8230900104f8dce3f2
Laura You-Li Chen <lauraylchen@hotmail.com>
2022-07-09 13:14:52 -0400

    Added transparent background for when modal pop out

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,6 +1,6 @@
 export default function Modal() {
   return(
-    <div className="modal-backdrop">
+    <div className="fixed top-0 left-0 w-full h-full bg-black/50">
       <div className="modal">
         <h2>10% Off Coupon Code!</h2>
         <p>Use the code NINJA10 at the checkout.</p>
