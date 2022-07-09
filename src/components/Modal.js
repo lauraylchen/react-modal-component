b6d3d609dcaf305a8e3c883c18f677ab1b1e78b6
Laura You-Li Chen <lauraylchen@hotmail.com>
2022-07-09 12:56:01 -0400

    Added content of the modal component

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,5 +1,10 @@
 export default function Modal() {
   return(
-    <div></div>
+    <div className="modal-backdrop">
+      <div className="modal">
+        <h2>10% Off Coupon Code!</h2>
+        <p>Use the code NINJA10 at the checkout.</p>
+      </div>
+    </div>
   )
 }
