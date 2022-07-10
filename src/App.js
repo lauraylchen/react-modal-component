3b6e94d8f914a1d0d637c8267e18decdee0f9992
Laura You-Li Chen <lauraylchen@hotmail.com>
2022-07-10 01:54:03 -0400

    Changed hard coded content of modal into using children props to have diff content using same component

--- src/App.js
+++ src/App.js
@@ -6,7 +6,10 @@ function App() {
       <h1 className="text-3xl font-bold py-10">
         MODAL COMPONENT
       </h1>
-      <Modal />
+      <Modal>
+        <h2 className="text-2xl font-bold pb-3">10% Off Coupon Code!</h2>
+        <p className="italic">Use the code <strong className="not-italic">NINJA10</strong> at the checkout.</p>
+      </Modal>
     </div>
   );
 }
--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,9 +1,8 @@
-export default function Modal() {
+export default function Modal({children}) {
   return(
     <div className="fixed top-0 left-0 w-full h-full bg-black/50">
       <div className="p-12 bg-white mx-auto my-52 max-w-xs sm:max-w-md rounded-lg">
-        <h2 className="text-2xl font-bold pb-3">10% Off Coupon Code!</h2>
-        <p className="italic">Use the code <strong className="not-italic">NINJA10</strong> at the checkout.</p>
+        {children}
       </div>
     </div>
   )
