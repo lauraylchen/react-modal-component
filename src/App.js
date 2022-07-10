283268ac77117c079fccae819dc3174c02af875b
Laura You-Li Chen <lauraylchen@hotmail.com>
2022-07-10 11:05:26 -0400

    Added useState to close modal component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
+import { useState } from "react";
 import Modal from "./components/Modal";
 
 function App() {
+  const [showModal, setShowModal] = useState(true)
+
+  const handleClose = () => {
+    setShowModal(false)
+  }
+
   return (
     <div className="text-center h-screen">
       <h1 className="text-3xl font-bold py-10">
         MODAL COMPONENT
       </h1>
-      <Modal>
+      {showModal && <Modal handleClose = {handleClose}>
         <h2 className="text-2xl font-bold pb-3">10% Off Coupon Code!</h2>
         <p className="italic">Use the code <strong className="not-italic">NINJA10</strong> at the checkout.</p>
-      </Modal>
+      </Modal>}
     </div>
   );
 }
--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,8 +1,9 @@
-export default function Modal({children}) {
+export default function Modal({children, handleClose}) {
   return(
     <div className="fixed top-0 left-0 w-full h-full bg-black/50">
       <div className="p-12 bg-white mx-auto my-52 max-w-xs sm:max-w-md rounded-lg">
         {children}
+        <button onClick={handleClose}>close</button>
       </div>
     </div>
   )
