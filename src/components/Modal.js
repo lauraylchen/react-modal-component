9ac4040c217ec6bebd20854c56964e9c597013c6
Laura You-Li Chen <lauraylchen@hotmail.com>
2022-07-10 18:53:24 -0400

    Change class to className

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -5,7 +5,7 @@ export default function Modal({children, handleClose}) {
         {children}
         <div className="absolute -top-3 -right-3">
           <button className="btn btn-circle btn-sm" onClick={handleClose}>
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" /></svg>
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" /></svg>
           </button>
         </div>
       </div>
